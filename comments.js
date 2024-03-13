85a917527dc495a9986550cf3a51970472849d2b
PhantomMist <106404019+PhantomMist@users.noreply.github.com>
2024-03-13 20:12:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,105 @@
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/comments', (req, res) => {
+  res.send('Comments');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Path: likes.js
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/likes', (req, res) => {
+  res.send('Likes');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+// create a route
+app.get('/comments', (req, res) => {
+  res.send('Comments');
+});
+
+// create a route
+app.get('/likes', (req, res) => {
+  res.send('Likes');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+// create a route
+app.get('/comments', (req, res) => {
+  res.send('Comments');
+});
+
+// create a route
+app.get('/likes', (req, res) => {
+  res.send('Likes');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+// create a route
